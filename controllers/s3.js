03ef54b05695e14e3a8c90deff0d0e6ae9214fed
kaganBerk1 <64051166+kaganBerk1@users.noreply.github.com>
2022-07-23 22:44:09 +0300

    Update s3.js

--- controllers/s3.js
+++ controllers/s3.js
@@ -7,6 +7,7 @@ const accessKeyId=process.env.AWS_ACCESS_KEY
 const secretAccessKey=process.env.AWS_SECRET_KEY
 
 const s3=new S3({
+     bucketName,
     region,
     accessKeyId,
     secretAccessKey
